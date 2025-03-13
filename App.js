aa514833b39305f0fd8c85867072ef4c0f1b9ba5
gjh70 <147407064+gjh70@users.noreply.github.com>
2025-03-13 22:59:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen.js';
 import IndustryHome from './src/screens/IndustryHome.js';
+import FarmerHome from './src/screens/FarmerHome.js';
 import IndustryTrans from './src/screens/IndustryTrans.js';
+import FarmerTrans from './src/screens/FarmerTrans.js';
 import IndustryHelp from './src/screens/IndustryHelp.js';
 import IndustryProfile from './src/screens/IndustryProfile.js';
 import IndustryProfileEdit from './src/screens/IndustryProfileEdit.js';
@@ -18,6 +20,8 @@ const App = () => {
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="IndustryHome" component={IndustryHome} />
+        <Stack.Screen name="FarmerHome" component={FarmerHome} />
+        <Stack.Screen name="FarmerTrans" component={FarmerTrans} />
         <Stack.Screen name="IndustryTrans" component={IndustryTrans} />
         <Stack.Screen name="IndustryHelp" component={IndustryHelp} />
         <Stack.Screen name="IndustryProfile" component={IndustryProfile} />
